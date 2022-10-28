ad989dfd0349d6e48019390ef8d7370dbecdbef3
Rocha134 <rochapakito@gmail.com>
2022-10-28 03:52:35 -0500

    Authentication

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+
 const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client');
 
 // 1
 const resolvers = {
-  Query: {
-    info: () => 'This is the API of a Hackernews Clone',
-    feed: async (parent, args, context) => {
-      return context.prisma.link.findMany()
-    } ,
-  },
-  Mutation: {
-    post: (parent, args, context, info) => {
-      const newLink = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      })
-      return newLink
-    },
-  },
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 const fs = require('fs');
@@ -28,16 +20,25 @@ const { info } = require('console');
 const prisma = new PrismaClient();
 
 // 3
+const { getUserId } = require('./utils');
+
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(
     path.join(__dirname, 'schema.graphql'),
     'utf8'
   ),
   resolvers,
-  context: {
-    prisma,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
   }
-})
+});
 
 server
   .listen()
---
+++ src/utils.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const APP_SECRET = 'GraphQL-is-aw3some';
+
+function getTokenPayload(token) {
+  return jwt.verify(token, APP_SECRET);
+}
+
+/*The getUserId function is a helper function that you’ll call
+ in resolvers which require authentication (such as post).*/
+function getUserId(req, authToken) {
+  if (req) {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.replace('Bearer ', '');
+      if (!token) {
+        throw new Error('No token found');
+      }
+      const { userId } = getTokenPayload(token);
+      return userId;
+    }
+  } else if (authToken) {
+    const { userId } = getTokenPayload(authToken);
+    return userId;
+  }
+
+  throw new Error('Not authenticated');
+}
+
+module.exports = {
+  APP_SECRET,
+  getUserId
+};
